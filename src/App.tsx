4c1f0fc4e07231f94ee3caa1471db018bae82929
Shane Garrity <shanedgarrity@gmail.com>
2019-01-20 02:24:12 -0500

    add tslint config, selectively disable import-name

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+// tslint:disable-next-line:import-name
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logoSvg from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
-  render() {
+  public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logoSvg} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
