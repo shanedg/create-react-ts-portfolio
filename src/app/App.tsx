0b9291f89f028e0e141a6d288c3811bdc9513c77
shane garrity <shanedgarrity@gmail.com>
2019-03-16 17:58:21 -0400

    feat: first pass route transition fly in/out

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,33 +13,62 @@ import Nav from './Nav';
 import Work from './Work';
 
 /**
- * Top-level App component.
+ * app main
  */
 class App extends Component {
 
-  constructor(props: any) {
-    super(props);
-  }
+  /**
+   * map numeric values to non-numeric string styles
+   * @param styles numeric style object
+   * @returns style object with transformed string style values
+   */
+  mapStyles = (styles: any) => {
+    return {
+      left: `${styles.left}vw`,
+      opacity: styles.opacity,
+    }
+  };
 
   public render() {
+    /**
+     * entering route style start state
+     */
+    const routeEnter = {
+      left: 200,
+      opacity: 0,
+    };
+
+    /**
+     * exiting route style end state
+     */
+    const routeLeave = {
+      left: -200,
+      opacity: 0,
+    };
+
+    /**
+     * active route styles
+     */
+    const routeActive = {
+      left: 0,
+      opacity: 1,
+    };
 
     return (
-      <div className="App">
-        <div className="App__inner">
-          <Header />
-          <Nav />
-          <AnimatedSwitch
-            // [todo] interesting transitions
-            atEnter={{ opacity: 0 }}
-            atLeave={{ opacity: 0 }}
-            atActive={{ opacity: 1 }}
-            className="switch-wrapper"
-          >
-            <Route exact path="/" component={About} />
-            <Route exact path="/contact/" component={Contact} />
-            <Route exact path="/work/" component={Work} />
-          </AnimatedSwitch>
-        </div>
+      <div className="app">
+        <Header />
+        <Nav />
+        <AnimatedSwitch
+          atEnter={routeEnter}
+          atLeave={routeLeave}
+          atActive={routeActive}
+          mapStyles={this.mapStyles}
+          className="app__switch"
+        >
+          <Route exact path="/" component={About} />
+          <Route exact path="/contact/" component={Contact} />
+          <Route exact path="/work/" component={Work} />
+        </AnimatedSwitch>
       </div>
     );
   }
