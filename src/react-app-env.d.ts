c6a191f6868165c8b074dd9c6611c90631498817
shane garrity <shanedgarrity@gmail.com>
2019-03-16 03:50:20 -0400

    feat: add type safety

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -4,3 +4,28 @@
  * No type safety for this imported module :/
  */
 declare module 'react-router-transition';
+
+type Dates = {
+  start: string | Date;
+  end: 'present' | Date;
+}
+
+type Details = {
+  bullets: string[];
+  title: string;
+}
+
+type DetailsProps = {
+  details: Details;
+}
+
+type Job = {
+  bullets: string[];
+  dates: Dates;
+  employerName: string;
+  roleTitle: string;
+}
+
+type JobProps = {
+  job: Job;
+}
