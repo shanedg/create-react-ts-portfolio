4fc35e5647d97e7556d3a8c42997f082c40025c0
shane garrity <shanedgarrity@gmail.com>
2019-04-05 23:36:40 -0400

    add read more scroll button

--- src/app/Work.tsx
+++ src/app/Work.tsx
@@ -1,18 +1,47 @@
 // tslint:disable-next-line:import-name
-import React from 'react';
+import React, { Component } from 'react';
 
 import Role from './Role';
 import roles from './work.roles';
 
-const Work = (props: any) => {
+class Work extends Component<any, {readMore: boolean}> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      readMore: false,
+    };
+
+    this.checkReadMoreOnScroll = this.checkReadMoreOnScroll.bind(this);
+  }
+
+  componentDidMount() {
+    this.checkReadMoreOnScroll();
+
+    const contentEl = document.querySelector('.work .content');
+    if (contentEl) {
+      contentEl.addEventListener('scroll', this.checkReadMoreOnScroll);
+    }
+  }
+
+  checkReadMoreOnScroll() {
+    const contentEl = document.querySelector('.work .content');
+    if (contentEl) {
+      const containerHeight = contentEl.clientHeight;
+      const fullScrollHeight = contentEl.scrollHeight;
+      const pixelsScrolled = contentEl.scrollTop;
+
+      this.setState({
+        readMore: (fullScrollHeight - pixelsScrolled) > containerHeight,
+      });
+    }
+  }
 
   /**
-   * build role component
    * @param job role data
    * @param index role index
-   * @returns role component populated with role data
    */
-  const buildRole = (job: Role, index: number) => {
+  buildRole(job: Role, index: number) {
     return (
       <Role
         key={index}
@@ -21,21 +50,49 @@ const Work = (props: any) => {
     )
   };
 
-  /**
-   * role components populated with role data
-   */
-  const builtRoles = roles.map(buildRole);
+  scrollLastRoleIntoView() {
+    const contentContainerSelector = '.work .content';
+    const lastRoleSelector = 'role:last-of-type';
 
-  return (
-    <article className="work">
-      <div className="heading">
-        <h2 className="heading__title">jobs</h2>
-      </div>
-      <section className="content">
-        {builtRoles}
-      </section>
-    </article>
-  );
+    if (window) {
+      const contentContainer = document.querySelector(contentContainerSelector);
+
+      if (contentContainer) {
+        const scrollBehaviorOptions: ScrollIntoViewOptions = {
+          behavior: 'smooth',
+        };
+
+        const lastRole = contentContainer.querySelector(lastRoleSelector);
+
+        (lastRole && contentContainer)
+          ? lastRole.scrollIntoView(scrollBehaviorOptions)
+          : null;
+      }
+    }
+  };
+
+  public render() {
+    const builtRoles = roles.map(this.buildRole);
+
+    return (
+      <article className="work">
+        <div className="heading">
+          <h2 className="heading__title">jobs</h2>
+        </div>
+        <section className="content">
+          {builtRoles}
+          {
+            this.state.readMore &&
+            <button
+              className="content__read-more"
+              onClick={this.scrollLastRoleIntoView}>
+              read more
+            </button>
+          }
+        </section>
+      </article>
+    );
+  }
 };
 
 export default Work;
